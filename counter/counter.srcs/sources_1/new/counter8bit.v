104df635b880da86d6ef1daf4991d474d40f8258
juan <jc.psp5@gmail.com>
2019-01-18 18:19:18 -0500

    Created counter sample. Didn't get very far so there's some i/o definitions but not much else. Therefore this isn't done at all.

---
+++ counter/counter.srcs/sources_1/new/counter8bit.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 01/18/2019 06:00:01 PM
+// Design Name: 
+// Module Name: counter8bit
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module counter8bit(
+    input clk,
+    input clr,
+    output reg [7:0] cnt,
+    output reg [7:0] led
+    );
+    
+    always @ (posedge clk)
+        begin
+            if (clr)
+                cnt <= 0;
+            else
+            cnt <= cnt + 1'b1;
+                
+        end
+endmodule
